7ffaefeb7317930e57ca8c956f50b4ecc2e6ea8e
Michael Dupuis Jr <michael.dupuis@dockyard.com>
2015-05-14 17:49:28 -0400

    Adds `assertInputHasClass` helper.

--- test-support/helpers/assertions.js
+++ test-support/helpers/assertions.js
@@ -1,5 +1,27 @@
 import Ember from 'ember';
 
+import {
+  findInputByLabel,
+  findLabelByText
+} from '../helpers/finders';
+
+export function assertCurrentUrl(assert, expectedUrl) {
+  return function() {
+    const currentUrl = currentURL();
+
+    assert.equal(currentUrl, expectedUrl);
+  };
+}
+
+export function assertInputHasClass(assert, label, klass) {
+  return function() {
+    const labelForInput = findLabelByText(label);
+    const input = findInputByLabel(labelForInput);
+
+    assert.equal(input.hasClass(klass), true, `${label} has class of ${klass}`);
+  };
+}
+
 export function assertValueEquals(assert, selector, expectedValue, context = null) {
   return function() {
     const value = find(selector, context).val();
@@ -14,12 +36,4 @@ export function assertValueNotEqual(assert, selector, expectedValue, context = n
 
     assert.notEqual(value, expectedValue);
   }
-}
-
-export function assertCurrentUrl(assert, expectedUrl) {
-  return function() {
-    const currentUrl = currentURL();
-
-    assert.equal(currentUrl, expectedUrl);
-  };
 }
